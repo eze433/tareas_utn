f79ac8f26cac93001e23736b637e8beb4cd2b1c0
ezequiel <ezekieldocampo@gmail.com>
2024-11-05 16:41:25 -0300

    tarea lambda

---
+++ lambda.py
@@ -0,0 +1,15 @@
+# sueldo = lambda s : s * 1.10
+
+# edad = lambda e : print('Es mayor') if e > 17 else print('Es menor')
+
+# pares = lambda n : print('Es par') if n % 2 == 0 else print('Es impar')
+
+# pos_neg = lambda n : print('Es positivo') if n > 0 else print('Es negativo')
+
+# descuento = lambda i : i * 0.90
+
+# doble = lambda n : n * 2
+
+masc_fem = lambda g : 'femenino' if g == 'f' else 'masculino' 
+
+print(masc_fem('m'))
